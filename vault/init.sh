33cdd743bd065797d23f2f20bb95e974b38a50fc
Jefmans <42002519+Jefmans@users.noreply.github.com>
2025-04-16 14:53:03 +0200

    update

--- vault/init.sh
+++ vault/init.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 echo "[vault-init] Waiting for Vault to be ready..."
+apt-get update && apt-get install -y curl
+
 
 until curl -s http://vault:8200/v1/sys/health | grep -q '"initialized":true'; do
   sleep 1
